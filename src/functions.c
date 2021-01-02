9052696075bf7f9cb3c762f71176135f3765dd26
Alexander Schwarze <shikami97@gmx.de>
2021-01-02 18:19:11 +0100

    Please don't look...

--- src/functions.c
+++ src/functions.c
@@ -493,7 +493,6 @@ void add_to_evaluation_list(tList * evaluation_list, char * evaluation_for, stru
     }
     xprintf(("Right after the if\n"));
     strcpy(tmp_evaluation->evaluation_for, evaluation_for);
-    xprintf(("Copied the name\nList: %s\n", time_evaluation_list));
     append_element(evaluation_list, tmp_evaluation);
     xprintf(("Appended the element\n"));
 }
