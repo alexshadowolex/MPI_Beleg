f5030e1f305de9ddb3da5597fd32415f51b62977
Alexander Schwarze <shikami97@gmx.de>
2021-01-17 13:17:01 +0100

    ABC

--- src/functions.c
+++ src/functions.c
@@ -369,7 +369,7 @@ tList * calc_SAD_values(tFile_data * ref_picture, tFile_data * other_picture, in
         int iterator_alltoall;
         int rank_has_best_SAD_value = 1;
         for(iterator_alltoall = 0; iterator_alltoall < size_receive_alltoall; iterator_alltoall++){
-            if(rank == 1) printf("Iterating over values. Block %i from worker_rank %i: %f; %i - %i\n", current_macro_block, iterator_alltoall, receive_alltoall[iterator_alltoall].value_SAD, receive_alltoall[iterator_alltoall].x_width, receive_alltoall[iterator_alltoall].y_height);
+            printf("Rank %i Iterating over values. Block %i from worker_rank %i: %f; %i - %i\n", rank, current_macro_block, iterator_alltoall, receive_alltoall[iterator_alltoall].value_SAD, receive_alltoall[iterator_alltoall].x_width, receive_alltoall[iterator_alltoall].y_height);
             if(iterator_alltoall == rank - 1){
                 continue;
             }
