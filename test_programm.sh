bf784a91efc13c76fe8fa3e56ff6451839cca485
Alexander Schwarze <shikami97@gmx.de>
2021-01-17 23:49:20 +0100

    Desperation

--- test_programm.sh
+++ test_programm.sh
@@ -92,9 +92,7 @@ do
             then
                 speed_up=0
             else
-                # speed_up=$(awk "BEGIN { pc=(${reference_times[$iterator_values-1]}-milliseconds)*100/${reference_times[$iterator_values-1]}; print pc}")
-                # percent=$(( 100 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]} + (1000 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]} % 10 >= 5 ? 1 : 0) ))
-                speed_up=$(echo "100 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]} + (1000 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]}" | bc) % 10 >= 5 ? 1 : 0)
+                speed_up=$((200 \* ${reference_times[$iterator_values-1]}-$milliseconds / ${reference_times[$iterator_values-1]} % 2 + 100 \* ${reference_times[$iterator_values-1]}-$milliseconds / ${reference_times[$iterator_values-1]}))
             fi
             speed_up_string="====> Speed up: $speed_up%"
         fi
