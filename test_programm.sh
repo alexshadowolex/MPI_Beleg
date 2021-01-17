ca150969c7213e22c8cd473dbd7748858dc0f461
Alexander Schwarze <shikami97@gmx.de>
2021-01-17 23:23:26 +0100

    MORE SEMANTIC FIX

--- test_programm.sh
+++ test_programm.sh
@@ -92,7 +92,7 @@ do
             then
                 speed_up=0
             else
-                speed_up=$(awk "BEGIN { (${reference_times[$iterator_values-1]}-milliseconds)*100/${reference_times[$iterator_values-1]}; print pc}")
+                speed_up=$(awk "BEGIN { pc=(${reference_times[$iterator_values-1]}-milliseconds)*100/${reference_times[$iterator_values-1]}; print pc}")
                 # speed_up=$(echo "(${reference_times[$iterator_values-1]}-milliseconds)*100/${reference_times[$iterator_values-1]}" | bc)
             fi
             speed_up_string="====> Speed up: $speed_up%"
