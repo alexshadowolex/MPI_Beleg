8d3aa5853d71604076394f870e65872163acd3aa
Alexander Schwarze <shikami97@gmx.de>
2021-01-17 22:43:37 +0100

    Debug

--- test_programm.sh
+++ test_programm.sh
@@ -88,6 +88,7 @@ do
         then
     	    reference_times[$iterator_values-1]=$milliseconds
         else
+            echo "${reference_times[$iterator_values-1]}"
             if [ $(echo "${reference_times[$iterator_values-1]} == 0" |bc -l) ]
             then
                 speed_up=0
