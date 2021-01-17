3d6a77810a69a72f302d993570b568c914a56437
Alexander Schwarze <shikami97@gmx.de>
2021-01-17 23:42:27 +0100

    I dont even know

--- test_programm.sh
+++ test_programm.sh
@@ -94,7 +94,7 @@ do
             else
                 # speed_up=$(awk "BEGIN { pc=(${reference_times[$iterator_values-1]}-milliseconds)*100/${reference_times[$iterator_values-1]}; print pc}")
                 # percent=$(( 100 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]} + (1000 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]} % 10 >= 5 ? 1 : 0) ))
-                speed_up=$(echo "100 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]} + (1000 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]} % 10 >= 5 ? 1 : 0)" | bc)
+                speed_up=$(echo "100 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]} + (1000 * (${reference_times[$iterator_values-1]}-milliseconds) / ${reference_times[$iterator_values-1]}" | bc) % 10 >= 5 ? 1 : 0)
             fi
             speed_up_string="====> Speed up: $speed_up%"
         fi
